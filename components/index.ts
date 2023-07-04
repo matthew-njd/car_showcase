e57fbd2c1df13726f0add77c6d22691c98bcd95a
Matthew D'Agostino <mnjdagostino@gmail.com>
2023-07-04 15:11:49 -0400

    SearchBar and Filter components added

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CustomButton from "./CustomButton";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufaturer from "./SearchManufacturer";
 
-export { Hero, Navbar, Footer, CustomButton };
+export {
+  Hero,
+  Navbar,
+  Footer,
+  CustomButton,
+  SearchBar,
+  CustomFilter,
+  SearchManufaturer,
+};
